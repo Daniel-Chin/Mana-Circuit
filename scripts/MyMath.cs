6816a76d9a893aa550b42206268c1bf8b4917d4b
Daniel Chin <daniel.chin@nyu.edu>
2023-02-11 21:34:34 +0800

    math seems correct

--- scripts/MyMath.cs
+++ scripts/MyMath.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
+// every number >= 1
+
 public enum TerminalType
 {
     NUMBER, OMEGA
@@ -29,7 +31,7 @@ public class Terminal
         switch (MyTerminalType)
         {
             case TerminalType.NUMBER:
-                return MyNumber.ToString("0.0");
+                return MyNumber.ToString("##.0");
             case TerminalType.OMEGA:
                 return "w";
             default:
@@ -100,15 +102,16 @@ public class Expression
                 doParenthesis = true;
             }
         }
-        if (doParenthesis) sB.Append('('); else sB.Append('<');
+        if (doParenthesis) sB.Append('(');  // else sB.Append('<');
         Left.BuildString(sB, MyOperator, true);
         sB.Append(' ');
         sB.Append(OperatorToChar(MyOperator));
         sB.Append(' ');
         Right.BuildString(sB, MyOperator, false);
-        if (doParenthesis) sB.Append(')'); else sB.Append('>');
+        if (doParenthesis) sB.Append(')');  // else sB.Append('>');
     }
-    static public char OperatorToChar(Operator op) {
+    static public char OperatorToChar(Operator op)
+    {
         switch (op)
         {
             case Operator.PLUS:
@@ -124,23 +127,25 @@ public class Expression
     public void ExpandAll()
     {
         if (IsTerminal) return;
+        Left.ExpandAll();
+        Right.ExpandAll();
         if (MyOperator == Operator.TIMES)
         {
-            if (! Left .IsTerminal && Left .MyOperator == Operator.PLUS)
+            if (!Left.IsTerminal && Left.MyOperator == Operator.PLUS)
             {
                 (Left, Right) = (Right, Left);
             }
-            if (! Right.IsTerminal && Right.MyOperator == Operator.PLUS)
+            if (!Right.IsTerminal && Right.MyOperator == Operator.PLUS)
             {
                 (MyOperator, Left, Right) = (
                     Operator.PLUS,
                     new Expression(Left, Operator.TIMES, Right.Left),
                     new Expression(Left, Operator.TIMES, Right.Right)
                 );
+                Left.ExpandAll();
+                Right.ExpandAll();
             }
         }
-        Left.ExpandAll();
-        Right.ExpandAll();
     }
 
     public Expression[] Top(Operator op)
@@ -162,63 +167,86 @@ public class Expression
     public static Expression Sample(double probToTerminate)
     {
         // Debug.Assert(probToTerminate > .5);    // converge
-        if (Shared.Rand.NextDouble() < probToTerminate) {
-            if (Shared.Rand.NextDouble() < .6) {
+        if (Shared.Rand.NextDouble() < probToTerminate)
+        {
+            if (Shared.Rand.NextDouble() < .6)
+            {
+                if (Shared.Rand.NextDouble() < .3)
+                {
+                    return Expression.ONE;
+                }
                 return new Expression(new Terminal(
-                    Shared.Rand.NextDouble()
+                    Shared.Rand.NextDouble() * 10 + 1
                 ));
-            } else {
+            }
+            else
+            {
                 return Expression.w;
             }
-        } else {
+        }
+        else
+        {
             double x = Shared.Rand.NextDouble();
             Operator op;
-            if (x < .4) {
+            if (x < .4)
+            {
                 op = Operator.PLUS;
-            } else if (x < .7) {
+            }
+            else if (x < .7)
+            {
                 op = Operator.TIMES;
-            } else {
+            }
+            else
+            {
                 op = Operator.POWER;
             }
             probToTerminate += (1 - probToTerminate) * .3;
             return new Expression(
-                Sample(probToTerminate), op, 
+                Sample(probToTerminate), op,
                 Sample(probToTerminate)
             );
         }
     }
 }
 
+public enum Rank
+{
+    FINITE, W_TO_THE_K, TWO_TO_THE_W, STACK_W
+}
+
 class Simplest
 {
-    public static readonly Simplest w = new Simplest(1);
-    public static readonly Simplest TwoToThew = new Simplest(3);
-    public static readonly Simplest ww = new Simplest(4);
-    public static readonly Simplest www = new Simplest(5);
-
-    public int Rank;
-    public bool IsNumber;
-    public bool IsWToTheK;
-    public bool IsWWWW;
+    public Rank MyRank;
     public double K;
     protected Simplest(
-        bool isNumber, bool isWToTheK, bool isWWWW, double k
+        Rank rank, double k
     )
     {
-        IsNumber = isNumber;
-        IsWToTheK = isWToTheK;
-        IsWWWW = isWWWW;
+        MyRank = rank;
         K = k;
-        if (isNumber) Rank = 0;
-        if (isWToTheK) Rank = 2;
-        if (isWWWW) Rank = 6;
-    }
-    protected Simplest(int rank)
-    {
-        Rank = rank;
-        if (rank == 1) K = 1;
-        if (rank == 4) K = 2;
-        if (rank == 5) K = 3;
+        switch (K)
+        {
+            case 0:
+                switch (rank)
+                {
+                    case Rank.W_TO_THE_K:
+                        MyRank = Rank.FINITE;
+                        K = 1;
+                        break;
+                    case Rank.STACK_W:
+                        throw new Shared.ObjectStateIllegal();
+                }
+                break;
+            case 1:
+                switch (rank)
+                {
+                    case Rank.STACK_W:
+                        MyRank = Rank.W_TO_THE_K;
+                        K = 1;
+                        break;
+                }
+                break;
+        }
     }
     public static Simplest FromExpression(
         Expression expression, bool verbose
@@ -228,15 +256,18 @@ class Simplest
         {
             TerminalType terminalType = expression.MyTerminal.MyTerminalType;
             if (terminalType == TerminalType.NUMBER)
-                return new Simplest(true, false, false, expression.MyTerminal.MyNumber);
+                return new Simplest(Rank.FINITE, expression.MyTerminal.MyNumber);
             if (terminalType == TerminalType.OMEGA)
-                return w;
+                return new Simplest(Rank.W_TO_THE_K, 1);
         }
-        Simplest left  = FromExpression(expression.Left , verbose);
+        Simplest left = FromExpression(expression.Left, verbose);
         Simplest right = FromExpression(expression.Right, verbose);
-        if (verbose) {
+        if (verbose)
+        {
             Console.Write(left);
+            Console.Write(' ');
             Console.Write(Expression.OperatorToChar(expression.MyOperator));
+            Console.Write(' ');
             Console.Write(right);
             Console.WriteLine();
         }
@@ -244,7 +275,8 @@ class Simplest
         Simplest result = Eval(
             left, expression.MyOperator, right
         );
-        if (verbose) {
+        if (verbose)
+        {
             Console.Write(" = ");
             Console.WriteLine(result);
         }
@@ -255,7 +287,10 @@ class Simplest
     )
     {
         Simplest a; Simplest b;
-        if (left.Rank < right.Rank)
+        if (
+            left.MyRank < right.MyRank
+            || (left.MyRank == right.MyRank && left.K < right.K)
+        )
         {
             a = left; b = right;
         }
@@ -266,84 +301,116 @@ class Simplest
         switch (op)
         {
             case Operator.PLUS:
-                if (a.Rank < b.Rank)
+                if (a.MyRank < b.MyRank)
                     return b;
-                if (a.IsNumber)
+                if (a.MyRank == Rank.FINITE)
                     return new Simplest(
-                        true, false, false, a.K + b.K
+                        Rank.FINITE, a.K + b.K
                     );
                 return b;
             case Operator.TIMES:
-                if (a.IsNumber)
+                switch (a.MyRank)
                 {
-                    if (b.IsNumber)
-                        return new Simplest(
-                            true, false, false, a.K * b.K
-                        );
-                    return b;
-                }
-                if (a.Rank == 1)
-                {
-                    if (b.Rank < 3)
-                        return new Simplest(
-                            false, true, false, 1 + b.K
-                        );
-                    return b;
-                }
-                if (a.Rank == 2)
-                {
-                    if (b.Rank < 3)
-                        return new Simplest(
-                            false, true, false, a.K + b.K
-                        );
-                    return b;
-                }
-                if (a.Rank >= 3)
-                {
-                    return b;
+                    case Rank.FINITE:
+                        if (b.MyRank == Rank.FINITE)
+                            return new Simplest(
+                                Rank.FINITE, a.K * b.K
+                            );
+                        return b;
+                    case Rank.W_TO_THE_K:
+                        if (b.MyRank == Rank.W_TO_THE_K)
+                            return new Simplest(
+                                Rank.W_TO_THE_K, a.K + b.K
+                            );
+                        return b;
+                    case Rank.TWO_TO_THE_W:
+                        return b;
+                    case Rank.STACK_W:
+                        return b;
                 }
                 break;
             case Operator.POWER:
-                if (a.IsNumber)
+                switch (left.MyRank)
                 {
-                    if (b.IsNumber)
-                        return new Simplest(
-                            true, false, false, Math.Pow(left.K, right.K)
-                        );
-                    if (b.Rank == 1)
-                    {
-                        if (a.K < 1) 
+                    case Rank.FINITE:
+                        if (right.MyRank == Rank.FINITE)
                             return new Simplest(
-                                true, false, false, 0
+                                Rank.FINITE, Math.Pow(left.K, right.K)
                             );
-                        if (a.K == 1) 
+                        if (left.K == 1)
+                            return left;
+                        if (right.MyRank == Rank.W_TO_THE_K)
+                        {
+                            if (right.K == 1)
+                                return new Simplest(
+                                    Rank.TWO_TO_THE_W, -1
+                                );
                             return new Simplest(
-                                true, false, false, 1
+                                Rank.STACK_W, 3
                             );
-                        return TwoToThew;
-                    }
-                    if (b.Rank < 5)
-                        return www;
-                    if (b.Rank == 5)
+                        }
+                        if (right.MyRank == Rank.TWO_TO_THE_W)
+                            return new Simplest(
+                                Rank.STACK_W, 3
+                            );
+                        if (right.MyRank == Rank.STACK_W)
+                            return new Simplest(
+                                Rank.STACK_W, right.K + 1
+                            );
+                        break;
+                    case Rank.W_TO_THE_K:
+                        if (right.MyRank == Rank.FINITE)
+                            return new Simplest(
+                                Rank.W_TO_THE_K, Math.Ceiling(left.K * right.K)
+                            );
+                        if (right.MyRank == Rank.W_TO_THE_K)
+                        {
+                            if (right.K == 1)
+                                return new Simplest(
+                                    Rank.STACK_W, 2
+                                );
+                            return new Simplest(
+                                Rank.STACK_W, 3
+                            );
+                        }
+                        if (right.MyRank == Rank.TWO_TO_THE_W)
+                            return new Simplest(
+                                Rank.STACK_W, 3
+                            );
+                        if (right.MyRank == Rank.STACK_W)
+                            return new Simplest(
+                                Rank.STACK_W, right.K + 1
+                            );
+                        break;
+                    case Rank.TWO_TO_THE_W:
+                        if (right.MyRank == Rank.FINITE)
+                            return left;
+                        if (right.MyRank == Rank.W_TO_THE_K)
+                            return new Simplest(
+                                Rank.STACK_W, 3
+                            );
+                        if (right.MyRank == Rank.TWO_TO_THE_W)
+                            return new Simplest(
+                                Rank.STACK_W, 3
+                            );
+                        if (right.MyRank == Rank.STACK_W)
+                            return new Simplest(
+                                Rank.STACK_W, right.K + 1
+                            );
+                        break;
+                    case Rank.STACK_W:
+                        if (right.MyRank == Rank.STACK_W)
+                            return new Simplest(
+                                Rank.STACK_W, Math.Max(
+                                    left.K, right.K + 1
+                                )
+                            );
+                        if (right.MyRank == Rank.FINITE)
+                            return left;
                         return new Simplest(
-                            false, false, true, 4
-                        );
-                    if (b.Rank == 6)
-                        return new Simplest(
-                            false, false, true, b.K + 1
-                        );
-                } else {
-                    if (b.Rank == 1)
-                        return ww;
-                    if (b.Rank < 5)
-                        return www;
-                    if (b.Rank == 5)
-                        return new Simplest(
-                            false, false, true, 4
-                        );
-                    if (b.Rank == 6)
-                        return new Simplest(
-                            false, false, true, b.K + 1
+                            Rank.STACK_W, Math.Max(
+                                3, left.K
+                            )
                         );
                 }
                 break;
@@ -351,39 +418,35 @@ class Simplest
         throw new Shared.ObjectStateIllegal();
     }
 
+    public override bool Equals(object obj)
+    {
+        Simplest that = obj as Simplest;
+        if (that == null) return false;
+        return (
+            MyRank == that.MyRank
+            && K == that.K
+        );
+    }
+    public override int GetHashCode()
+    {
+        return MyRank.GetHashCode() ^ K.GetHashCode();
+    }
+
     public override string ToString()
     {
-        if (this == w)
+        switch (MyRank)
         {
-            return "w";
-        }
-        else if (this == TwoToThew)
-        {
-            return "2^w";
-        }
-        else if (this == ww)
-        {
-            return "w^w";
-        }
-        else if (this == www)
-        {
-            return "w^w^w";
-        }
-        else
-        {
-            if (IsNumber)
-            {
-                return K.ToString();
-            }
-            else if (IsWToTheK)
-            {
+            case Rank.FINITE:
+                return K.ToString("##.0");
+            case Rank.W_TO_THE_K:
+                if (K == 1)
+                    return "w";
                 return $"w^{K}";
-            }
-            else if (IsWWWW)
-            {
-                return new String('w', (int)K);
-            }
-            else
+            case Rank.TWO_TO_THE_W:
+                return "2^w";
+            case Rank.STACK_W:
+                return String.Join("", Enumerable.Repeat("w^", ((int)K) - 1)) + "w";
+            default:
                 throw new Shared.ObjectStateIllegal();
         }
     }
---
+++ scripts/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("begin");
+            Console.WriteLine("end");
+            Console.ReadKey();
+        }
+
+        static void TestMath() {
+            for (int i = 0; i < 32; i++)
+            {
+                Console.WriteLine("=== ONE ITER ===");
+                Expression ww = new Expression(
+                    Expression.w, Operator.POWER, Expression.w
+                );
+                // Expression wp1 = new Expression(
+                //     Expression.w, Operator.PLUS, Expression.ONE
+                // );
+                // Expression wp1tw = new Expression(
+                //     wp1, Operator.TIMES, Expression.w
+                // );
+                // Expression wp1twp1 = new Expression(
+                //     wp1, Operator.TIMES, wp1
+                // );
+                // Console.WriteLine(wp1twp1);
+                // wp1twp1.ExpandAll();
+                // Console.WriteLine(wp1twp1);
+                Expression e = Expression.Sample(0);
+                // Expression e = new Expression(
+                //     new Expression(new Terminal(3.4)), Operator.POWER, ww
+                // );
+                Console.WriteLine(e);
+                Console.WriteLine();
+                Simplest s = Simplest.FromExpression(e, true);
+                Console.WriteLine();
+                e.ExpandAll();
+                Console.WriteLine(e);
+                Console.WriteLine();
+                Simplest es = Simplest.FromExpression(e, false);
+                if (! s.Equals(es)) {
+                    Console.WriteLine("Oh NOOOO");
+                    throw new ApplicationException();
+                }
+                Console.WriteLine(s);
+                Console.WriteLine();
+                // Console.ReadKey();
+            }
+        }
+    }
+}
