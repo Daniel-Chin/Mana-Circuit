196a4fdb09f70e8ef3523c0d491391e561d6e0c1
Daniel Chin <daniel.chin@nyu.edu>
2023-02-21 13:06:44 +0800

    enemy

---
+++ game/scripts/Enemy.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public class Enemy : Node2D
+{
+    public RichTextLabel label;
+    public override void _Ready()
+    {
+        label = GetNode<RichTextLabel>("RichTextLabel");
+    }
+
+    public void SetHP(Simplest simplest)
+    {
+        label.BbcodeText = $"[center]{MathBB.Build(simplest)}[/center]";
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+}
