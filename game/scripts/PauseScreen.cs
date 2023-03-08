51f86fee9514fb85759f0a94e2346c6b9af5ffb0
Daniel Chin <daniel.chin@nyu.edu>
2023-03-08 18:45:52 +0800

    mention save location

--- game/scripts/PauseScreen.cs
+++ game/scripts/PauseScreen.cs
@@ -1,5 +1,6 @@
-using Godot;
 using System;
+using System.Text;
+using Godot;
 
 public class PauseScreen : WindowDialog
 {
@@ -11,6 +12,13 @@ public class PauseScreen : WindowDialog
         GetNode<Button>("VBox/HBox/QuitButton").Connect(
             "pressed", this, "SaveAndQuit"
         );
+        StringBuilder sB = new StringBuilder();
+        sB.Append("# Game save \n");
+        sB.Append("Location: \n");
+        sB.Append(Godot.OS.GetUserDataDir());
+        sB.Append("\n");
+        sB.Append("Delete all files there to reset the game. \n");
+        GetNode<TextEdit>("VBox/TextHBox/TextEdit").Text += sB.ToString();
     }
 
     public void Resume() {
